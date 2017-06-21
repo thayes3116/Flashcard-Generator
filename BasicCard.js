0afbc71f7b54d543d6a6523fbe86fdbc6b9f6642
thayes3116 <timhayes3116@gmail.com>
2017-06-21 17:10:14 -0400

    finished product.  added scope safe constructors

--- BasicCard.js
+++ BasicCard.js
@@ -5,7 +5,7 @@ var frontArray = [],
 
 //constructor to build basic cards
 function BasicCard(front,back){
-
+	if (this instanceof BasicCard){
 		this.front = front;
 		this.back = back;
 
@@ -13,12 +13,16 @@ function BasicCard(front,back){
 			frontArray.push(this.front);
 			backArray.push(this.back);
 		}
+	}else{
+
+		return new BasicCard(front,back)
+	}	
 }
 //create cards using constructor
-var george = new BasicCard("Who was the president (the first one from 1789-1797) who lived in Mount Vernon?", "George Washington");	
-var andrew = new BasicCard("What president succeeded Abraham Lincoln when he was assassinated?", "Andrew Jackson");
-var brown = new BasicCard("Whose raid on Harper's Ferry led to further tensions before the war?", "John Brown");
-var phila = new BasicCard(" In what city was the Declaration of Independence signed?", "Philadelphia");
+var george = BasicCard("Who was the president (the first one from 1789-1797) who lived in Mount Vernon?", "George Washington"),	
+	andrew = BasicCard("What president succeeded Abraham Lincoln when he was assassinated?", "Andrew Jackson"),
+	brown = BasicCard("Whose raid on Harper's Ferry led to further tensions before the war?", "John Brown"),
+	phila = BasicCard(" In what city was the Declaration of Independence signed?", "Philadelphia");
 
 //package data for export
 fullBasicArray.push(frontArray);
--- ClozeCard.js
+++ ClozeCard.js
@@ -6,26 +6,34 @@ var questionArray = [],
 //constructor to build cloze cards
 function ClozeCard(question, cloze){
 
-	this.question = question;
-	this.cloze = cloze;
-	this.preparedQuestion = (this.question).replace(this.cloze, "......");
+	if(this instanceof ClozeCard){
 
- 	this.push = () => {
+		this.question = question;
+		this.cloze = cloze;
+		this.preparedQuestion = (this.question).replace(this.cloze, "......");
 
-		questionArray.push(this.preparedQuestion);
-		clozeArray.push(this.cloze);
+	 	this.push = () => {
 
-	};
+			questionArray.push(this.preparedQuestion);
+			clozeArray.push(this.cloze);
+
+		};
+
+	}else{
+
+		return new ClozeCard(question, cloze);
+
+	}
 }
 
 //create cards using constructor
-var jamestown = new ClozeCard("Jamestown, the first permanent English settlement, was founded in 1607.", "Jamestown");	
-var louisiana = new ClozeCard("President Thomas Jefferson purchased the Louisiana Territory from France in 1803.", "Louisiana");
-var yorktown = new ClozeCard("The British defeat at Yorktown, Virginia by George Washington’s troops signaled the end of the American Revolution.", "Yorktown");
-var gettysburg = new ClozeCard("The Battle of Gettysburg was the turning point in the Civil War for the North.  Confederate troops were forced to retreat and never invaded the North again.", "Gettysburg");
-var separation = new ClozeCard("Separation of Powers is a system in which each branch of government has its own powers.", "Separation");
-var democracy = new ClozeCard("A Democracy is a form of government that is run for and by the people, giving people the supreme power.", "Democracy");
-var monroe = new ClozeCard("The Monroe Doctrine was a foreign policy statement delivered by President James Monroe stating that 1) the U.S. would not interfere in European affairs, and 2) that the western hemisphere was closed to colonization and/ or interference by European nations.", "Monroe");
+var jamestown = ClozeCard("Jamestown, the first permanent English settlement, was founded in 1607.", "Jamestown"),	
+	louisiana = ClozeCard("President Thomas Jefferson purchased the Louisiana Territory from France in 1803.", "Louisiana"),
+	yorktown = ClozeCard("The British defeat at Yorktown, Virginia by George Washington’s troops signaled the end of the American Revolution.", "Yorktown"),
+	gettysburg = ClozeCard("The Battle of Gettysburg was the turning point in the Civil War for the North.  Confederate troops were forced to retreat and never invaded the North again.", "Gettysburg"),
+	separation = ClozeCard("Separation of Powers is a system in which each branch of government has its own powers.", "Separation"),
+	democracy = ClozeCard("A Democracy is a form of government that is run for and by the people, giving people the supreme power.", "Democracy"),
+	monroe = ClozeCard("The Monroe Doctrine was a foreign policy statement delivered by President James Monroe stating that 1) the U.S. would not interfere in European affairs, and 2) that the western hemisphere was closed to colonization and/ or interference by European nations.", "Monroe");
 
 //package data for export
 jamestown.push();
